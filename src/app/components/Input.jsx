bae4bea600025cf3074e231aa56e9b5b326fe0da
Purnama S Rahayu <purnamasrahayu@gmail.com>
2023-11-25 10:05:06 +0700

    style button update

--- src/app/components/Input.jsx
+++ src/app/components/Input.jsx
@@ -11,7 +11,7 @@ export const Input = () => {
     <div className=' flex flex-col w-full lg:w-1/3 space-y-2 m-auto lg:min-w-fit'>
       <input className=' p-2 rounded-xl focus:outline-none border-b-2 border-r-2 border-rose-400' placeholder="Nama bunga..." type="text" value={content} onChange={onContentChange} />
       <input className=' p-2 rounded-xl focus:outline-none border-b-2 border-r-2 border-rose-400' placeholder="Url gambar dari unsplash.com..." type='text' value={additionalData} onChange={onImageChange} />
-      {isOnEdit ? <div className=' flex justify-center space-x-2'><button className=' w-1/3 bg-rose-300 py-2 rounded-xl hover:bg-red-400' onClick={() => updateData(id_update)}>Update</button><button className=' w-1/3 bg-rose-300 py-2 rounded-xl hover:bg-red-400' onClick={cancelEdit}>Cancel</button></div> : <button className=' bg-rose-300 py-2 rounded-xl hover:bg-red-400' onClick={addData}>Create</button>}
+      {isOnEdit ? <div className=' flex justify-between space-x-2'><button className=' w-[49%] bg-rose-300 py-2 rounded-xl hover:bg-red-400' onClick={() => updateData(id_update)}>Update</button><button className=' w-[49%] bg-rose-300 py-2 rounded-xl hover:bg-red-400' onClick={cancelEdit}>Cancel</button></div> : <button className=' bg-rose-300 py-2 rounded-xl hover:bg-red-400' onClick={addData}>Create</button>}
 
     </div>
   )
